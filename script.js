3c32fdde57f828a7606bf38c751460aa03b2377f
Ali65005377 <aloshsmezher@gmail.com>
2022-12-14 20:03:34 +0300

    done

--- script.js
+++ script.js
@@ -10,28 +10,67 @@ let random_grade_div = document.getElementById("random-grade");
 
 
 
+grades.pop();
+
+grades.push(99);
 
 
 
+for (let i = 0; grades.length > i; i++) {
+  all_grades_div.innerHTML += grades[i] + " ";
+}
 
+let randomindex = Math.floor(Math.random() * grades.length);
+
+random_grade_div.innerHTML = grades[randomindex];
 
 
 /* الجزء الثالث من التمرين */
 
 let course = {
   name: "UniCODE",
-  track : "",
-  studentsCount : 0,
-  location : "Kuwait University"
+  track: "",
+  studentsCount: 0,
+  location: "Kuwait University"
 }
 
-//Change studentsCount,track property here
 
+//Change studentsCount,track property here
+course.studentsCount = 40;
+course.track = "web";
 
 
 //Console log here
 
-
+console.log(`Hello my name is ali, I'm in ${course.track} at ${course.name}`);
 
 //Create student object here
 
+let student = {
+  name: "ali",
+  university: "AUK"
+}
+
+let unicodes = [
+  {
+    track: "game",
+    location: "Kuwait University"
+  },
+  {
+    track: "python",
+    location: "AUM"
+  },
+  {
+    track: "cyber",
+    location: "GUST"
+  }
+];
+
+
+unicodes.forEach(element => {
+
+  console.log(unicodes[0].track);
+  console.log(unicodes[1].track);
+  console.log(unicodes[2].track);
+
+});
